37725410ed88af6f7d9f5c6788bbdba504baeff7
mobile08092018 <kakvospitat@gmail.com>
2021-08-05 14:55:54 +0300

    logOut true/false

--- src/main/header/Header.tsx
+++ src/main/header/Header.tsx
@@ -1,22 +1,32 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './Header.module.css'
 import {ButtonComponent} from "../../components/ButtonComponent";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {AuthLoginThunk, LogOutThunk} from "../../reducers/LoginReducer";
 import {Redirect} from "react-router-dom";
 import {ButtonAC} from "../../reducers/ButtonReducer";
+import {AppStoreType} from "../../redux/store";
 
 export const Header = () => {
+    let [LogOUT, setLogOUT] = useState(false)
+    let LoginData = useSelector<AppStoreType, any[]>(state => state.login)
     let dispatch = useDispatch();
     let [colorButton, setColorButton] = useState('')
     const changeButtonColor = (titleColor: string) => {
         setColorButton(titleColor)
     }
-    const logOiutHandler=()=>{
+    const logOiutHandler = () => {
         dispatch(LogOutThunk())
         dispatch(ButtonAC('Login'))
+        setLogOUT(false)
         // return <Redirect to={'/login'}/>
     }
+
+    useEffect(() => {
+        if (LoginData.length > 0) {
+            setLogOUT(true)
+        }
+    }, [LoginData])
 
     return (
         <div className={styles.header}>
@@ -34,7 +44,7 @@ export const Header = () => {
                              colorButton={colorButton}/>
             <ButtonComponent title={'RecoveryPassword'} to={'/newPas'} changeButtonColor={changeButtonColor}
                              colorButton={colorButton}/>
-            <span onClick={logOiutHandler} className={styles.logOut}>LogOUT</span>
+            {LogOUT && <span onClick={logOiutHandler} className={styles.logOut}>LogOUT</span>}
         </div>
     )
 }
